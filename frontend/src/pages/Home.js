8ba706f268e22998277c6c4896b42b2f91706d8c
tmpk13 <167717442+tmpk13@users.noreply.github.com>
2025-03-18 23:54:04 -0700

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import AuthContext from '../context/AuthContext';
