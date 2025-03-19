80aef2107afb73eb2315026a1aa360832e055692
timpupk <timpupk@pdx.edu>
2025-03-19 15:40:43 -0700

    update

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -6,16 +6,14 @@ const ChatMessage = ({ message, currentUserId }) => {
   
   return (
     <div className={`message ${isOwnMessage ? 'own-message' : 'other-message'}`}>
-      <div className="message-content">
-        {!isOwnMessage && (
-          <div className="message-sender">
-            {message.sender.firstName} {message.sender.lastName}
-          </div>
-        )}
-        <div className="message-text">{message.content}</div>
-        <div className="message-time">
-          {new Date(message.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+      {!isOwnMessage && (
+        <div className="message-sender">
+          {message.sender.firstName} {message.sender.lastName}
         </div>
+      )}
+      <div className="message-text">{message.content}</div>
+      <div className="message-time">
+        {new Date(message.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
       </div>
     </div>
   );
